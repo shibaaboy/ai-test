0d12ca08249c42a49389267430276f5e7fe921cd
Alex <43315031+shibaaboy@users.noreply.github.com>
2025-02-24 23:37:35 +0300

    Create cubNumbers.ts

---
+++ cubNumbers.ts
@@ -0,0 +1,9 @@
+// Function to add two numbers 
+const cubNumbers = (a) => { 
+  return a ** 2; 
+}
+
+console.log('test', cubNumbers(5));
+console.log('test2', cubNumbers(10));
+console.log('test3', cubNumbers(20));
+console.log('test4', cubNumbers(10));
